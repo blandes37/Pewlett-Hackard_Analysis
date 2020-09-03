346b5eae336cea46f137fc25af4654adcce023d2
Ben <68405490+blandes37@users.noreply.github.com>
2020-09-03 14:29:15 -0400

    almost done

--- SQL_mod.sql
+++ SQL_mod.sql
@@ -143,4 +143,18 @@ ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
 
+-- Skill Drill
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_pull
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales')
+
+
 
